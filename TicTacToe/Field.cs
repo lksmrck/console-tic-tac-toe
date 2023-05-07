a08d0990684d4042c1e326335b81b20604354ac2
LukasMercak <lukasthevicious@gmail.com>
2023-05-07 14:13:01 +0200

    first commit

---
+++ TicTacToe/Field.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace TicTacToe
+{
+	public class Field
+	{
+
+		public char[][] PlayingField { get; private set; }
+		public int FieldResolution { get; private set; }
+
+
+		// Constructor
+		public Field(int fieldResolution)
+		{
+            PlayingField = CreateField(fieldResolution);
+			FieldResolution = fieldResolution;
+        }
+
+		// Methods
+
+		public char[][] CreateField(int fieldResolution)
+		{
+
+			var field = new char[fieldResolution][];
+
+			for (int i = 0; i < fieldResolution; i++)
+			{
+				field[i] = new char[fieldResolution];
+
+				for (int j = 0; j < fieldResolution; j++)
+				{
+					field[i][j] = 'X';
+				}
+
+			}
+
+			return field;
+
+		}
+
+		public void PrintField()
+		{
+			Console.WriteLine(" |1|2|3|");
+			for (int i = 0;  i < FieldResolution;  i++)
+			{
+				Console.Write($"{ i + 1 }|");
+				for (int j = 0; j < FieldResolution; j++)
+				{
+					Console.Write($"{PlayingField[i][j]}|");
+				}
+
+				Console.WriteLine();
+
+			}
+
+		}
+
+
+	}
+}
+
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+
+var field = new TicTacToe.Field(3);
+
+field.PrintField();
